9b626921c6cebd81edf4223d485f1d641722a967
Andrew Stucki <andrew.stucki@govdelivery.com>
2016-01-13 10:48:32 -0600

    update

--- server/models.js
+++ server/models.js
@@ -1,26 +1,32 @@
+var fs = require('fs');
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 var promise = require('promise');
 var validator = require('validator');
 var mandrill = require('mandrill-api/mandrill');
 var hat = require('hat');
+var handlebars = require('handlebars');
 var _ = require('underscore');
 
 var config = require("./config");
 
+console.log(process.env.MANDRILL_APIKEY);
 var mailer = new mandrill.Mandrill(process.env.MANDRILL_APIKEY);
+var templates = {html: {}, plain: {}};
 
-var generateEmail = function(fields) {
-  var emailTemplate = {
-    "from_email": "andrew.stucki@gmail.com",
-    "from_name": "Voting App",
-    "headers": {
-      "Reply-To": "andrew.stucki+reply@gmail.com"
-    }
-  };
-
-  return _.extend({}, emailTemplate, fields);
-};
+_.each(["html", "plain"], function(templateType) {
+  var dirname = __dirname + "/emails/" + templateType + "/";
+  fs.readdir(dirname, function(err, filenames) {
+    if (err) throw new Error("Unable to read template directory");
+    _.each(filenames, function(filename) {
+      fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+        var templateName = filename.split(".")[0];
+        if (err) throw new Error("Unable to read templates");
+        templates[templateType][templateName] = handlebars.compile(content);
+      });
+    });
+  });
+});
 
 // initialize mongo
 mongoose.Promise = promise;
@@ -133,34 +139,43 @@ userSchema.statics.logout = function(token) {
   });
 };
 
-userSchema.methods.confirmationEmail = function() {
-  var user = this;
-
-  return generateEmail({
-    html: "Click <a href=\"http://localhost:3000/confirm/" + user.confirmationToken +
-      "\">here</a> to confirm your account",
-    text: "Go to http://localhost:3000/confirm/" + user.confirmationToken +
-      " to confirm your account",
-    subject: "Account confirmation",
+userSchema.methods.generateEmail = function(subject, templateName, context) {
+  console.log(templateName, subject, templates.plain[templateName]);
+  console.log(templates.plain[templateName](context));
+  return {
+    from_email: "andrew.stucki@gmail.com",
+    from_name: "Voting App",
+    headers: {
+      "Reply-To": "andrew.stucki+reply@gmail.com"
+    },
+    html: templates.html[templateName](context),
+    text: templates.plain[templateName](context),
+    subject: subject,
     to: [{
       "email": user.email,
       "type": "to"
     }]
-  });
+  };
 };
 
 userSchema.methods.sendConfirmation = function() {
   console.log("sending confirmation");
   var user = this;
+  var confirmationLink = "http://localhost:3000/confirm/" + user.confirmationToken;
 
   return new promise(function(resolve, reject) {
     if (user.confirmed) return reject("User email already confirmed");
+    console.log(user.generateEmail("Confirm your Voting App email address!", "confirm", {confirmationLink: confirmationLink}));
     mailer.messages.send({
-      message: user.confirmationEmail(),
+      message: user.generateEmail("Confirm your Voting App email address!", "confirm", {confirmationLink: confirmationLink}),
       async: false
     }, function(result) {
+      console.log("sent message");
+      console.log(result);
       return resolve(result);
     }, function(err) {
+      console.log("error sending message");
+      console.log(err);
       return reject(err);
     });
   });
